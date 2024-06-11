ed7df95e695c6a77b6527166e0f8ce98446b2f53
Anumjawaid <anumjawaid99@gmail.com>
2024-06-12 02:44:36 +0500

    INITIALIZATION

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+export function Home(){
+    return(
+        <>
+        <>Header</>
+        <>Introduction</>
+        <>Filters</>
+        <>Profile</>
+        <>Properties</>
+        <Footer></Footer>
+        </>
+    )
+}
